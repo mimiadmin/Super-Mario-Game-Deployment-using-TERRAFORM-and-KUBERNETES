035bf916478ac50d391621d997311dbade7dd141
miriamjoso1974@gmail.com <miriamjoso1974@gmail.com>
2024-10-17 15:38:18 -0800

    my second commit

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
+    bucket = "mario-bucket-mimi" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
